b53cb93d67acc8ef849a58d21eb2b2f44cb7b445
nodgd <dcc1234@qq.com>
2017-07-25 23:24:47 +0800

    我记得什么都没改但是git让我更新一下我就更新之后再看看改了哪里嘛

---
+++ GarbageMayor/TableHeader.h
@@ -0,0 +1,19 @@
+#ifndef TABLEHEADER_H_
+#define TABLEHEADER_H_
+
+#include "DataType.h"
+
+namespace GarbageMayor
+{
+    /*
+    表头类。
+    */
+    class TableHeader
+    {
+        
+        
+        
+    };
+}
+
+#endif // TABLEHEADER_H_
