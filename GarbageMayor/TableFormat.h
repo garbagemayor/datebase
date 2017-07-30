c336f994332d6af8eba33c84eb108f94937a3cbf
nodgd <dcc1234@qq.com>
2017-07-30 22:17:47 +0800

    换个文件名真tm麻烦

---
+++ GarbageMayor/TableFormat.h
@@ -0,0 +1,31 @@
+#ifndef TABLEHEADER_H_
+#define TABLEHEADER_H_
+
+#include "DataType.h"
+
+namespace GarbageMayor
+{
+    /*
+    单元格式类。
+    */
+    class TableFormatCell
+    {
+    private:
+        DTenum type;
+        char column_name[TF;
+        
+        
+    }
+    
+    /*
+    行格式。
+    */
+    class TableRowFormat
+    {
+        
+        
+        
+    };
+}
+
+#endif // TABLEHEADER_H_
